0ac84619ddafeb2dede0ecb3faeef83f2771a8f0
PriyanshuChandel <87568275+PriyanshuChandel@users.noreply.github.com>
2021-09-20 22:41:18 +0800

    Create Simple-Dictionary.py

---
+++ Simple-Dictionary.py
@@ -0,0 +1,8 @@
+dict1 = {"abandoned": "having been deserted or left.",
+         "phenomenal": "remarkable or exceptional, especially exceptionally good.",
+         "chaos": "complete disorder and confusion.", "entrepreneur":
+             "a person who sets up a business or businesses, taking on financial risks in the hope of profit.",
+         "ancestor": "a person, typically one more remote than a grandparent, from whom one is descended"}
+print("Enter the Word")
+inpusr = input()
+print("Meaning of", inpusr, "is", dict1[inpusr])
