651849d897596a5734041be88c57fb1be504e2e1
PriyanshuChandel <87568275+PriyanshuChandel@users.noreply.github.com>
2021-11-14 19:34:05 +0800

    Update newspaper_reader.py

--- newspaper_reader.py
+++ newspaper_reader.py
@@ -9,18 +9,18 @@ def newspaper_reader(str):
 if __name__ =='__main__':
     newspaper_reader("Starting to read India's top headline for today")
     url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=2aab9df35b5842fab8833c3f23871bad"
-    num = 0
+    num = 1
     news_json = requests.get(url).text
     news_py = json.loads(news_json)
     arts = news_py["articles"]
     values = news_py["totalResults"]
     for article in arts:
-        print(f"Here is the headline: {article['title']}\nIf you want to read more, please refer to this link: "
-              f"{article['url']}")
+        print(f"Here is the headline-{num}: {article['title']}\nIf you want to read more, please refer to this link: "
+              f"{article['url']}\n")
         newspaper_reader(article['title'])
         num = num + 1
         compare = values
         if num < compare:
             newspaper_reader("Moving to the next headline, listen carefully.")
-        if num == compare:
+        if num == compare-1:
             newspaper_reader("Thanks for listening")
